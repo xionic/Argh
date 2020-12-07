102aa5ba161fb8858fa00a7d63019a0f88f6309f
Nick Spargo <intergalacticlollypopman@hotmail.com>
2020-12-07 13:37:17 +0000

    Make importable by composer

--- src/ValidationException.php
+++ src/ValidationException.php
@@ -8,8 +8,9 @@ class ValidationException extends \Exception {
 	function __construct(String $reason, String $offendingArg, $offendingValue) {
 		if(is_object($offendingValue)){
 			$offendingValue = " object of class " . get_class($offendingValue);
-		}
-		else{
+		} else if (is_array($offendingValue)){
+			$offendingValue = "array";
+		} else {
 			$offendingValue = "'$offendingValue'";
 		}
 		$reason .= ". Argument: '$offendingArg' Value: $offendingValue"; 
